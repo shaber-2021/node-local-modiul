dd085f43f931df56c925aa39a9d165149ebd181f
shaber <shaberahmad2019s@gmail.com>
2022-07-28 23:53:17 +0300

    this is node local modiul

---
+++ app.js
@@ -0,0 +1,6 @@
+const {myname,myage,home} = require('./index');
+
+
+console.log(myname())
+console.log(myage());
+console.log(home)
---
+++ index.js
@@ -0,0 +1,19 @@
+const myname=()=>{
+    return 'my name is shaber ';
+}
+
+const myage=()=>{
+    return 'my age is 32';
+}
+
+const home = 'my home is khulna';
+
+// exports.name = myname;
+// exports.age=myage;
+// exports.home=home;
+
+module.exports={
+    myname,
+    myage,
+    home
+}
